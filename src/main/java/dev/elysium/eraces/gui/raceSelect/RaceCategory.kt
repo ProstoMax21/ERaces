af427db79248b2cc2f2ef0db635906c34fdf5dec
ProstoMax21 <m4ksimus21@gmail.com>
2026-07-15 05:52:04 +0200

    Add displayName to RaceCategory data class

--- src/main/java/dev/elysium/eraces/gui/raceSelect/RaceCategory.kt
+++ src/main/java/dev/elysium/eraces/gui/raceSelect/RaceCategory.kt
@@ -1,7 +1,11 @@
 package dev.elysium.eraces.gui.raceSelect
 
 data class RaceCategory(
+
     val id: String,
-    val name: String,
-    val races: MutableList<RacePage>
+
+    val displayName: String,
+
+    val races: MutableList<RacePage> = mutableListOf()
+
 )
